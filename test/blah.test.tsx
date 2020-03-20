7edb99b5f1b881eb02fec9a1d9bc6bb7802d6948
Kaho Cheung <kaho@elefanttraks.com>
2020-03-20 22:59:46 +1100

    Fix test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import SheetMusic from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<SheetMusic />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
