28511d3103d915b2061660b726f2d92b7e841fa5
Luke Dearnley <luked@phm.gov.au>
2020-04-02 15:51:05 +1100

    trying to get bpm changes working

--- src/SheetMusic.tsx
+++ src/SheetMusic.tsx
@@ -52,6 +52,9 @@ const SheetMusic: React.FunctionComponent<Props> = ({
     parseOnly: Function;
     TimingCallbacks: any;
   }>();
+  const tune = React.useRef<{
+    setTiming: Function;
+  }>();
 
   React.useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -69,7 +72,7 @@ const SheetMusic: React.FunctionComponent<Props> = ({
         const noteList = parseJSON(json, { bpm });
 
         // Now render actual score:
-        const tune = abcjs.current.renderAbc(id, notation, {
+        tune.current = abcjs.current.renderAbc(id, notation, {
           add_classes: true,
           scale,
           staffwidth: staffWidth,
@@ -82,95 +85,109 @@ const SheetMusic: React.FunctionComponent<Props> = ({
             : {}),
         });
 
-        timer.current = new abcjs.current.TimingCallbacks(tune[0], {
-          qpm: bpm,
-          beatSubdivisions: json[0].lines[0].staff[0].meter.value[0].num, // 4,
-          beatCallback: (beatNumber, totalBeats, totalTime) => {
-            if (typeof onBeat === 'function') {
-              onBeat(beatNumber, totalBeats, totalTime);
-            }
-          },
-          lineEndCallback: (info) => {
-            if (typeof onLineEnd === 'function') {
-              onLineEnd(info);
-            }
-          },
-          eventCallback: (event) => {
-            if (typeof onEvent === 'function') {
-              if (event === null) {
-                onEvent(null);
-              } else {
-                // Event.midiPitches isn't working, so we need to work out pitch from ABC notation.
-                // We use the event's array of start and end positions (positions in the notation string)
-                // that point out which notes are being played at this point in time (ie event).
-                // Each pair of these is formed into a unique key for our array of notes: noteList.
-                const allNotes = event.startCharArray.map((_, index) => {
-                  const startChar = event.startCharArray[index];
-                  const endChar = event.endCharArray[index];
-                  return noteList[`s${startChar}e${endChar}`];
-                });
-                // now smoosh all the notes into one array and remove nulls (rests)
-                const charNotes = []
-                  .concat(...allNotes)
-                  .filter((char) => Boolean(char));
-                if (typeof onEvent === 'function') {
-                  onEvent({
-                    ...event,
-                    notes: charNotes,
-                  });
+        if (tune && tune.current) {
+          console.log(tune.current[0]);
+          timer.current = new abcjs.current.TimingCallbacks(tune.current[0], {
+            qpm: bpm,
+            beatSubdivisions: json[0].lines[0].staff[0].meter.value[0].num, // 4,
+            beatCallback: (beatNumber, totalBeats, totalTime) => {
+              if (typeof onBeat === 'function') {
+                onBeat(beatNumber, totalBeats, totalTime);
+                if (tune && tune.current) {
+                  tune.current[0].setTiming(bpm);
                 }
               }
-            }
-
-            if (!event) {
-              return null;
-            }
-
-            const notes = document.getElementsByClassName('abcjs-note');
-            const rests = document.getElementsByClassName('abcjs-rest');
-            const lyrics = document.getElementsByClassName('abcjs-lyric');
-
-            // Remove all highlighted notes
-            for (let note of [].slice.call(notes)) {
-              note.classList.remove('abcjs-note_playing');
-            }
-
-            // Remove all highlighted rests
-            for (let rest of [].slice.call(rests)) {
-              rest.classList.remove('abcjs-rest_playing');
-            }
-
-            // Remove all highlighted lyrics
-            for (let lyric of [].slice.call(lyrics)) {
-              lyric.classList.remove('abcjs-lyric_playing');
-            }
-
-            // Highlight current playing lyric/rest/note
-            event.elements.forEach((nodes) => {
-              nodes.forEach((node) => {
-                const classes = node.className.baseVal;
-                let type;
-
-                if (classes.indexOf('abcjs-lyric') > -1) {
-                  type = 'lyric';
-                } else if (classes.indexOf('abcjs-rest') > -1) {
-                  type = 'rest';
-                } else if (classes.indexOf('abcjs-note') > -1) {
-                  type = 'note';
+            },
+            lineEndCallback: (info) => {
+              if (typeof onLineEnd === 'function') {
+                onLineEnd(info);
+              }
+            },
+            eventCallback: (event) => {
+              if (typeof onEvent === 'function') {
+                if (event === null) {
+                  onEvent(null);
+                } else {
+                  // Event.midiPitches isn't working, so we need to work out pitch from ABC notation.
+                  // We use the event's array of start and end positions (positions in the notation string)
+                  // that point out which notes are being played at this point in time (ie event).
+                  // Each pair of these is formed into a unique key for our array of notes: noteList.
+                  const allNotes = event.startCharArray.map((_, index) => {
+                    const startChar = event.startCharArray[index];
+                    const endChar = event.endCharArray[index];
+                    return noteList[`s${startChar}e${endChar}`];
+                  });
+                  // now smoosh all the notes into one array and remove nulls (rests)
+                  const charNotes = []
+                    .concat(...allNotes)
+                    .filter((char) => Boolean(char));
+                  if (typeof onEvent === 'function') {
+                    onEvent({
+                      ...event,
+                      notes: charNotes,
+                    });
+                  }
                 }
+              }
 
-                node.classList.add(`abcjs-${type}_playing`);
+              if (!event) {
+                return null;
+              }
+
+              const notes = document.getElementsByClassName('abcjs-note');
+              const rests = document.getElementsByClassName('abcjs-rest');
+              const lyrics = document.getElementsByClassName('abcjs-lyric');
+
+              // Remove all highlighted notes
+              for (let note of [].slice.call(notes)) {
+                note.classList.remove('abcjs-note_playing');
+              }
+
+              // Remove all highlighted rests
+              for (let rest of [].slice.call(rests)) {
+                rest.classList.remove('abcjs-rest_playing');
+              }
+
+              // Remove all highlighted lyrics
+              for (let lyric of [].slice.call(lyrics)) {
+                lyric.classList.remove('abcjs-lyric_playing');
+              }
+
+              // Highlight current playing lyric/rest/note
+              event.elements.forEach((nodes) => {
+                nodes.forEach((node) => {
+                  const classes = node.className.baseVal;
+                  let type;
+
+                  if (classes.indexOf('abcjs-lyric') > -1) {
+                    type = 'lyric';
+                  } else if (classes.indexOf('abcjs-rest') > -1) {
+                    type = 'rest';
+                  } else if (classes.indexOf('abcjs-note') > -1) {
+                    type = 'note';
+                  }
+
+                  node.classList.add(`abcjs-${type}_playing`);
+                });
               });
-            });
 
-            return null;
-          },
-        });
+              return null;
+            },
+          });
+        }
       }
     }
     /* eslint-disable */
-  }, [JSON.stringify(notation)]);
+  }, [JSON.stringify(notation), bpm]);
   /* eslint-enable */
+
+  // uncomment this below and remove bpm from the above useEffect, also comment out lines 96-98.
+  // React.useEffect(() => {
+  //   if (tune && tune.current) {
+  //     tune.current[0].setTiming(bpm);
+  //     console.log(bpm, tune.current[0].getBpm(bpm));
+  //   }
+  // }, [bpm]);
 
   React.useEffect(() => {
     if (timer && timer.current) {
